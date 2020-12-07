de54a8cb6b276a05bef1a33226350238831b897e
Tadas Petra <connectwithamateur@gmail.com>
2020-12-07 15:21:57 -0600

    Books displayed on home screen
    - Homescreen converted to a sliverlist. And can once you add the books to your account, they will appear on the home screen

--- convene/lib/pages/home/view/home_page.dart
+++ convene/lib/pages/home/view/home_page.dart
@@ -1,7 +1,11 @@
+import 'package:convene/config/palette.dart';
+import 'package:convene/domain/book_repository/book_repository.dart';
 import 'package:convene/pages/home/widgets/add_drawer.dart';
 import 'package:convene/pages/home/widgets/books.dart';
 import 'package:convene/pages/home/widgets/clubs.dart';
 import 'package:convene/pages/home/widgets/menu_drawer.dart';
+import 'package:convene/providers/book_provider.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter/material.dart';
 
 /// If you want to use Hooks, you need to create a separate stateless widget for that,
@@ -18,37 +22,71 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final GlobalKey<ScaffoldState> _homeScaffoldKey = GlobalKey();
+  List<BookModel> _currentBooks;
+  @override
+  Future<void> didChangeDependencies() async {
+    // TODO: maybe using a future builder or some riverpod thing is better
+    _currentBooks =
+        await context.read(bookRepositoryProvider).getCurrentBooks();
+    setState(() {});
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _homeScaffoldKey,
-      body: Center(
-        child: Column(
-          children: [
-            SafeArea(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.menu),
-                    onPressed: () {
-                      _homeScaffoldKey.currentState.openDrawer();
-                    },
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.add),
-                    onPressed: () {
-                      _homeScaffoldKey.currentState.openEndDrawer();
-                    },
-                  ),
-                ],
-              ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            leading: IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                _homeScaffoldKey.currentState.openDrawer();
+              },
             ),
-            Books(),
-            Clubs(),
-          ],
-        ),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.add),
+                onPressed: () {
+                  _homeScaffoldKey.currentState.openEndDrawer();
+                },
+              ),
+            ],
+            backgroundColor: Palette.lightGrey,
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text("books"),
+                    SizedBox(
+                      height: 40,
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Books(_currentBooks ?? <BookModel>[]),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    SizedBox(
+                      height: 80,
+                    ),
+                    Text("clubs"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
       drawer: const MenuDrawer(),
       endDrawer: const AddDrawer(),
